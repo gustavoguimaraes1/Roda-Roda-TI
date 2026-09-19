5df9f498f94ddcd49bb858e642e3998161600d93
Gustavo  Guimarães <gustavoguimaraes@Mac-mini-de-Innovalab-7.local>
2026-09-19 18:02:47 -0300

    Adiciona sorteio do Roda Roda TI

---
+++ roda_roda_ti.py
@@ -0,0 +1,40 @@
+from random import choice
+
+linguagens = [
+    "C",
+    "C++",
+    "C#",
+    "Java",
+    "Ruby",
+    "Python",
+    "Pascal",
+    "Swift",
+    "JavaScript",
+    "Go",
+    "Dart",
+    "Kotlin",
+    "Haskell",
+    "OCaml",
+    "Clojure",
+    "Scala",
+    "Lua",
+    "R",
+    "Rust",
+    "Elixir",
+    "TypeScript",
+    "PHP"
+]
+
+print("=" * 45)
+print("RODA RODA TI")
+print("=" * 45)
+
+print('\nSilvio Linus: "QUEM QUER UMA 5090?!"')
+print('\nSilvio Linus: "OLHA A DDR5 PASSANDO!"')
+
+linguagem_sorteada = choice(linguagens)
+
+print("\nA roleta está girando...")
+print(f"\nLINGUAGEM SORTEADA: {linguagem_sorteada}")
+
+print('\nSilvio Linus: "1010 1010 111000! 0101 0101!"')
